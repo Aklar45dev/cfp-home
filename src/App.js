7cd0485faed1a66807f24961d5bb168a528e166d
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2022-05-03 09:59:24 -0400

    btns

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const openApp = url => {
+      window.location.href = url
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home-logo">
+        <img src="./logo.png"></img>
+        <a href='' />
+      </div>
+      <h1>Applications CFPVD</h1>
+      <div className='btn-container'>
+        <button className='btn' onClick={() => openApp('https://main.d2chkat9k8gdjb.amplifyapp.com/')}>Boussole</button>
+        <button className='btn' onClick={() => openApp('https://main.d3h0me83mnk2rv.amplifyapp.com/')}>Entretien</button>
+        <button className='btn' onClick={() => openApp('https://main.dpuszw4zf9ttg.amplifyapp.com/')}>Formation Mine</button>
+      </div>
     </div>
   );
 }
