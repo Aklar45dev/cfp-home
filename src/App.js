8717ac5afd8e4cb4f748ee22d13dfeba574f0158
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2022-05-09 13:34:18 -0400

    ui

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ function App() {
   return (
     <div className="App">
       <div className="home-logo">
-        <img src="./logo.png"></img>
-        <a href='' />
+        <img src="./images.jpg"></img>
       </div>
-      <h1>Applications CFPVD</h1>
+      <div className="logo">
+        <img src="./logo.png" />
+      </div>
+      <h1>Applications</h1>
       <div className='btn-container'>
-        <button className='btn' onClick={() => openApp('https://main.d2chkat9k8gdjb.amplifyapp.com/')}>Boussole</button>
-        <button className='btn' onClick={() => openApp('https://main.d3h0me83mnk2rv.amplifyapp.com/')}>Entretien</button>
-        <button className='btn' onClick={() => openApp('https://main.dpuszw4zf9ttg.amplifyapp.com/')}>Formation Mine</button>
+        <button className='btn' onClick={() => openApp('https://main.d2chkat9k8gdjb.amplifyapp.com/')}><img src="https://cfpvd-bucket.s3.ca-central-1.amazonaws.com/boussole/Image15.png"/><p>Boussole</p></button>
+        <button className='btn' onClick={() => openApp('https://main.d3h0me83mnk2rv.amplifyapp.com/')}><img src="./images2.jpg"/><p>Entretien</p></button>
+        <button className='btn' onClick={() => openApp('https://main.dpuszw4zf9ttg.amplifyapp.com/')}><img src="./images3.jpg"/><p>Formation Mine</p></button>
       </div>
     </div>
   );
